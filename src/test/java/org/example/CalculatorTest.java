9009720e6e8ac78db4599745f145ba74f39aab1e
ib <ip-oe@31-16-baranovskiy.ru>
2024-09-01 15:02:42 +0300

    Инициализация проекта

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -3,16 +3,20 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculatorTest {
 
     @Test
     void sumMethodShouldReturnSumOfTwoIntegers() {
         int a = 1;
         int b = 2;
+
         Calculator calc = new Calculator();
 
-        int sum = calc.sum(1, 2);
+        int sum = calc.sum(a, b);
 
-        Assertions.assertEquals(a + b, sum);
+        assertEquals(a + b, sum);
+        org.assertj.core.api.Assertions.assertThat(sum).isEqualTo(a + b);
     }
 }
